5b1aba27a6dc2c2030ca0d87a5cd7fa45b92ede9
Joshua Ditton <joshuaditton@internode.on.net>
2023-04-25 19:13:46 +1000

    Children mock data

---
+++ src/components/dashboard/mock/Children.ts
@@ -0,0 +1,22 @@
+import Child from 'src/types/Child';
+
+const children: Child[] = [
+  {
+    id: 1,
+    name: 'Amiya Boss',
+    born: '2021-04-13',
+    family_id: 1,
+    avatar: 'http://localhost:8000/storage/images/avatars/children/1.png',
+  },
+  {
+    id: 2,
+    name: 'Ethyl Boss',
+    born: '2022-08-10',
+    family_id: 1,
+    avatar: 'http://localhost:8000/storage/images/avatars/children/2.png',
+  },
+];
+
+const child: Child = children[0];
+
+export { children as default, child };
