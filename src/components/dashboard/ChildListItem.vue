f77a5fd55a42c684507e54515c4764bdbdd2619c
Joshua Ditton <joshuaditton@internode.on.net>
2023-04-25 21:51:54 +1000

    Child List Item implementation

---
+++ src/components/dashboard/ChildListItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-avatar>
+    <img class="child-img" :src="child.avatar" />
+  </q-avatar>
+  <span data-test="age" class="child-age">
+    {{ child.age }}
+  </span>
+</template>
+
+<script setup lang="ts">
+import { PropType } from 'vue';
+//types
+import Child from 'src/types/Child';
+
+const props = defineProps({
+  child: {
+    required: true,
+    type: Object as PropType<Child>,
+  },
+});
+</script>
