e4ca30041954d64f565ccf050a1ad53171c83f14
Joshua Ditton <joshuaditton@internode.on.net>
2023-05-01 17:12:46 +1000

    Better test names DRY principle

--- src/components/dashboard/ChildListItem.test.ts
+++ src/components/dashboard/ChildListItem.test.ts
@@ -5,40 +5,41 @@ import { describe, expect, it } from 'vitest';
 
 //components
 import ChildListItem from './ChildListItem.vue';
-//types
-import Child from 'src/types/Child';
 //mock data
-import { child as baby } from './mockData/Children';
-
-function factory(child: Child): VueWrapper {
-  return mount(ChildListItem, {
-    props: {
-      child: child,
-    },
-  });
-}
+import { child } from './mockData/Children';
 
 describe('When given a child property', () => {
-  it('should show an avatar', () => {
-    const wrapper = factory(baby);
-    const image = wrapper.get('img');
+  let wrapper: VueWrapper;
 
-    expect(image.attributes('src')).toBe(baby.avatar);
+  const getImage = () => wrapper.get('img');
+  const getAge = () => wrapper.get('[data-test="age"]');
+  const getAvatar = () => wrapper.get('[data-test="avatar"]');
+
+  const createComponent = () => {
+    wrapper = mount(ChildListItem, {
+      props: {
+        child: child,
+      },
+    });
+  };
+
+  it('should show an avatar img', () => {
+    createComponent();
+
+    expect(getImage().attributes('src')).toBe(child.avatar);
   });
 
   it('should show the childs age', () => {
-    const wrapper = factory(baby);
-    const age = wrapper.get('[data-test="age"]');
+    createComponent();
 
-    expect(age.text()).toBe(baby.age);
+    expect(getAge().text()).toBe(child.age);
   });
 
   it('emits selected event if its avatar is clicked', async () => {
-    const wrapper = factory(baby);
-    const avatar = wrapper.get('[data-test="avatar"]');
+    createComponent();
 
-    await avatar.trigger('clicked');
+    await getAvatar().trigger('clicked');
 
-    expect(wrapper.emitted().selected[0]).toEqual([baby.id]);
+    expect(wrapper.emitted().selected[0]).toEqual([child.id]);
   });
 });
