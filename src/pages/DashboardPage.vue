875e7f9528547b471d32cf9698c451e1ddf3279c
Joshua Ditton <joshuaditton@internode.on.net>
2023-05-04 18:34:49 +1000

    Dashboard Page init

--- src/pages/DashboardPage.vue
+++ src/pages/DashboardPage.vue
@@ -1,9 +1,25 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import DashboardFamilyVue from 'src/components/dashboard/DashboardFamily.vue';
+import Family from 'src/types/Family';
+import Adult from 'src/types/Adult';
+import Child from 'src/types/Child';
 import { families as familiesTestData } from 'src/data/Families';
 
 const families = ref(familiesTestData);
+
+function handleAddChild(family: Family) {
+  console.log(family.id);
+}
+function handleInviteAdult(family: Family) {
+  console.log(family.id);
+}
+function handleChildSelected(child: Child) {
+  console.log(child);
+}
+function handleAdultSelected(adult: Adult) {
+  console.log(adult);
+}
 </script>
 
 <template>
@@ -13,6 +29,10 @@ const families = ref(familiesTestData);
       v-for="family in families"
       :family="family"
       :key="family.id"
+      @addChild="handleAddChild"
+      @inviteAdult="handleInviteAdult"
+      @childSelected="handleChildSelected"
+      @adultSelected="handleAdultSelected"
     />
   </q-page>
 </template>
--- test/vitest/__tests__/pages/DashboardPage.test.ts
+++ test/vitest/__tests__/pages/DashboardPage.test.ts
@@ -1,12 +1,16 @@
 import { useQuasar } from 'quasar';
 useQuasar();
 
+import { mount } from '@vue/test-utils';
 import { describe, it, expect } from 'vitest';
 import {
   VueRouterMock,
   createRouterMock,
   injectRouterMock,
 } from 'vue-router-mock';
+
+import DashboardFamilyVue from 'src/components/dashboard/DashboardFamily.vue';
+import { family } from 'src/data/Families';
 
 describe('', () => {
   it.todo('');
