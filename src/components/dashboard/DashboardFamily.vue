43904447ad39d1cc9bebe8c93f3709a7a691735e
Joshua Ditton <joshuaditton@internode.on.net>
2023-05-04 13:27:25 +1000

    Family for dashboard implementation

---
+++ src/components/dashboard/DashboardFamily.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { PropType } from 'vue';
+import { QBtn } from 'quasar';
+import AdultListVue from './AdultList.vue';
+import ChildListVue from './ChildList.vue';
+
+import Family from 'src/types/Family';
+
+defineProps({
+  family: {
+    type: Object as PropType<Family>,
+    required: true,
+  },
+  manager: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
+
+<template>
+  <div class="dashboard-family">
+    <h2 data-test="family-heading">{{ family.name }}</h2>
+    <QBtn data-test="add-child-button">add child</QBtn>
+    <AdultListVue class="dashboard-adults" :adults="family.adults" />
+    <ChildListVue class="dashboard-children" :children="family.children" />
+  </div>
+</template>
+
+<style lang="sass">
+.dashboard-family
+  display: flex
+  flex-direction: column
+  justify-content: left
+</style>
