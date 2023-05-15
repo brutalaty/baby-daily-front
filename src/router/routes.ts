16af8c6ec0904d0286841a8693377a45c375df72
Joshua Ditton <joshuaditton@internode.on.net>
2023-05-16 08:56:47 +1000

    scaffolding some routes

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -9,6 +9,36 @@ const routes: RouteRecordRaw[] = [
         path: '',
         component: () => import('pages/testing/UnitUnderTest.vue'),
       },
+      {
+        name: 'create-family',
+        path: 'create-family',
+        component: () => import('pages/IndexPage.vue'),
+      },
+      {
+        name: 'invite-adult',
+        path: 'families/:family-id/invite',
+        component: () => import('pages/IndexPage.vue'),
+      },
+      {
+        name: 'invite-adult',
+        path: 'families/:family-id/invite',
+        component: () => import('pages/IndexPage.vue'),
+      },
+      {
+        name: 'adult',
+        path: 'families/:family-id/adults/:adult-id',
+        component: () => import('pages/IndexPage.vue'),
+      },
+      {
+        name: 'child',
+        path: 'children/:child-id',
+        component: () => import('pages/IndexPage.vue'),
+      },
+      {
+        name: 'create-child',
+        path: 'families/:family-id',
+        component: () => import('pages/IndexPage.vue'),
+      },
     ],
   },
 
