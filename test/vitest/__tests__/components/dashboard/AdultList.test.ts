da95ad67ec2ec8d3b88e72970c98fc17edfbee59
Joshua Ditton <joshuaditton@internode.on.net>
2023-05-16 16:24:40 +1000

    refactor AdultList tests Cleaner, easier to read

--- test/vitest/__tests__/components/dashboard/AdultList.test.ts
+++ test/vitest/__tests__/components/dashboard/AdultList.test.ts
@@ -1,41 +1,42 @@
 import { installQuasar } from '@quasar/quasar-app-extension-testing-unit-vitest';
 installQuasar();
 import { VueWrapper, shallowMount } from '@vue/test-utils';
-import { describe, expect, it } from 'vitest';
+import { describe, expect, it, beforeEach } from 'vitest';
 
 import AdultList from 'src/components/dashboard/AdultList.vue';
 import adults from 'src/data/Adults';
 
-describe('When given a list of adults', () => {
+const ADULT_ITEM = '[data-test="adult-list-item"]';
+
+describe('Adult List', () => {
   let wrapper: VueWrapper;
 
-  const findAdultListItems = () =>
-    wrapper.findAllComponents('[data-test="adult-list-item"]');
+  beforeEach(() => createComponent());
 
-  const createComponent = (options: object = {}) => {
+  const createComponent = () => {
     wrapper = shallowMount(AdultList, {
       props: {
         adults: adults,
       },
-      ...options,
     });
   };
 
-  it('should display correct number of AdultListItems', () => {
-    createComponent();
-    expect(findAdultListItems()).toHaveLength(2);
+  const findAdultListItems = () => wrapper.findAllComponents(ADULT_ITEM);
+
+  it('should display correct number of Adults', () => {
+    expect(findAdultListItems()).toHaveLength(adults.length);
   });
 
-  it('should re emit an adults selected event', () => {
-    createComponent();
-    const items = findAdultListItems();
+  describe('when receiving a selected event', () => {
+    it('emits that selected event', () => {
+      const items = findAdultListItems();
+      expect(wrapper.emitted().selected).toBeUndefined();
 
-    expect(wrapper.emitted().selected).toBeUndefined();
+      items[0].vm.$emit('selected', adults[0]);
+      items[1].vm.$emit('selected', adults[1]);
 
-    items[0].vm.$emit('selected', adults[0]);
-    items[1].vm.$emit('selected', adults[1]);
-
-    expect(wrapper.emitted().selected[0]).toEqual([adults[0]]);
-    expect(wrapper.emitted().selected[1]).toEqual([adults[1]]);
+      expect(wrapper.emitted().selected[0]).toEqual([adults[0]]);
+      expect(wrapper.emitted().selected[1]).toEqual([adults[1]]);
+    });
   });
 });
