b1a01d498ffc6988b505c13a6e0c7b8ba06c5bff
Joshua Ditton <joshuaditton@internode.on.net>
2023-05-16 11:33:05 +1000

    refactor ChildList tests cleaner, easier to read

--- test/vitest/__tests__/components/dashboard/ChildList.test.ts
+++ test/vitest/__tests__/components/dashboard/ChildList.test.ts
@@ -1,16 +1,17 @@
 import { installQuasar } from '@quasar/quasar-app-extension-testing-unit-vitest';
 installQuasar();
 import { VueWrapper, shallowMount } from '@vue/test-utils';
-import { describe, expect, it } from 'vitest';
+import { describe, expect, it, beforeEach } from 'vitest';
 
 import ChildList from 'src/components/dashboard/ChildList.vue';
 import children from 'src/data/Children';
 
-describe('When given a a list of children', () => {
+const CHILD_ITEM = '[data-test="child-item"]';
+
+describe('Child List', () => {
   let wrapper: VueWrapper;
 
-  const findChildListItems = () =>
-    wrapper.findAllComponents('[data-test="child-item"]');
+  beforeEach(() => createComponent());
 
   const createComponent = () => {
     wrapper = shallowMount(ChildList, {
@@ -20,20 +21,21 @@ describe('When given a a list of children', () => {
     });
   };
 
-  it('dispays the correct number of ChildListItems', () => {
-    createComponent();
+  const findChildListItems = () => wrapper.findAllComponents(CHILD_ITEM);
 
+  it('renders the correct number of children', () => {
     expect(findChildListItems()).toHaveLength(children.length);
   });
 
-  it('should re emit any childs selected events', () => {
-    createComponent();
-    const childItems = findChildListItems();
+  describe('when receiving a selected event', () => {
+    it('emits that selected event', () => {
+      const childItems = findChildListItems();
 
-    childItems[0].vm.$emit('selected', children[0]);
-    childItems[1].vm.$emit('selected', children[1]);
+      childItems[0].vm.$emit('selected', children[0]);
+      childItems[1].vm.$emit('selected', children[1]);
 
-    expect(wrapper.emitted().selected[0]).toStrictEqual([children[0]]);
-    expect(wrapper.emitted().selected[1]).toStrictEqual([children[1]]);
+      expect(wrapper.emitted().selected[0]).toStrictEqual([children[0]]);
+      expect(wrapper.emitted().selected[1]).toStrictEqual([children[1]]);
+    });
   });
 });
